a3e1e4286a610e852e425628485d8b6f0bb4fb65
Daniela Araujo <daniela.scaraujo@outlook.com>
2024-10-07 12:08:10 -0300

    commit inicial

---
+++ src/exercicio.js
@@ -0,0 +1,15 @@
+"use strict";
+// Criando a Função de Multiplicação
+function multiplicar(numero1, numero2) {
+    return numero1 * numero2;
+}
+// Utilizando a Função
+let resultadoMultiplicacao = multiplicar(25, 5);
+console.log(resultadoMultiplicacao);
+// Criando a Função de Saudação
+function saudacao(nome) {
+    return "Olá, " + nome;
+}
+// Utilizando a Função
+let mensagemDeSaudacao = saudacao("Marília");
+console.log(mensagemDeSaudacao);
---
+++ src/exercicio.ts
@@ -0,0 +1,17 @@
+// Criando a Função de Multiplicação
+function multiplicar(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+// Utilizando a Função
+let resultadoMultiplicacao = multiplicar(25, 5);
+console.log(resultadoMultiplicacao);
+
+// Criando a Função de Saudação
+function saudacao(nome: string): string {
+    return "Olá, " + nome;
+}
+
+// Utilizando a Função
+let mensagemDeSaudacao = saudacao("Marília");
+console.log(mensagemDeSaudacao);
